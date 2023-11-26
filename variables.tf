04c7b4a202cd9215a8e3bf324f2ae48504e3d551
mmarcal <augustoribeiro.marcal@gmail.com>
2023-11-26 19:50:46 +0000

    Refactoring

--- variables.tf
+++ variables.tf
@@ -14,31 +14,3 @@ variable "TIMDb_API_KEY" {
   description = "API Key for TIMDb"
   type        = string
 }
-
-# variable "bucket_name" {
-#   description = "Name of the S3 bucket to create"
-#   type        = string
-# }
-
-# variable "ami_id" {
-#   description = "AMI ID for the EC2 instances"
-#   type        = string
-# }
-
-# variable "asg_min_size" {
-#   description = "Minimum size of the Auto Scaling Group"
-#   type        = number
-#   default     = 1
-# }
-
-# variable "asg_max_size" {
-#   description = "Maximum size of the Auto Scaling Group"
-#   type        = number
-#   default     = 2
-# }
-
-# variable "asg_desired_capacity" {
-#   description = "Desired capacity of the Auto Scaling Group"
-#   type        = number
-#   default     = 2
-# }
